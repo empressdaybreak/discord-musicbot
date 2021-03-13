4b656ae61f81321b659c9e0003d5a966db550b23
Gihoon Lee <as616111@gmail.com>
2021-03-14 01:01:59 +0900

    feature: 음성 재생 기능 추가

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,39 @@
-import { Client } from 'discord.js';
+import {Client, VoiceConnection} from 'discord.js';
+import ytdl from 'ytdl-core-discord';
 
 const client = new Client();
+let voiceConnection: VoiceConnection | null | undefined = null;
+
+
+client.on('ready',      () => {
+    console.log(`${client.user!.tag}에 로그인하였습니다!`);
+});
+
+client.on('message', async msg => {
+    if (msg.content === '들어와') {
+        await msg.reply('죄송합니다,,, 흑흑,,,');
+        voiceConnection = await msg.member?.voice.channel?.join();
+    }
+
+    if(msg.content === '나가줘') {
+        await msg.reply('나가볼께요');
+        voiceConnection?.disconnect();
+    }
+
+    if (voiceConnection && msg.content.startsWith("!play")) {
+        await msg.reply('네? 섹스요????');
+        voiceConnection.play(
+            await ytdl('https://www.youtube.com/watch?v=D00hlkW0u3U', { filter: "audioonly" }),
+            {
+                type: 'opus',
+                highWaterMark: 50,
+            }
+        );
+    }
+    if (msg.content === '핑') {
+        msg.reply('퐁!');
+    }
+});
+
+client.login('NjU1NDIwNjM0ODkyODYxNDkz.XfT2CA.qbyPl2k9u2NfFg5pD4L2_CnZQqo');
+
