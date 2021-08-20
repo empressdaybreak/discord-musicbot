87be8bf8589c8eaada0ab919c19b749ef2891a13
daybreak <as616111@gmail.com>
2021-08-20 18:17:28 +0900

    feature: 기존 썼던 쿠로 목록을 중복만 제거했던 기능이었던 부분에서 각 지역 토벌전 이름에 맞춰서 나눠서 출력하고 중복을 없애도록 함.

--- src/server.ts
+++ src/server.ts
@@ -20,7 +20,46 @@ const client = new Client();
 let RandomResult: string[] = [];
 
 // 쿠로 만들기용 배열
-let kuroArr: string[] = [];
+let kuroArr: any = [];
+
+let sinArr: string[] = [];
+let changArr: string[] = [];
+let hongArr: string[] = [];
+let darkArr: string[] = [];
+
+const kuroListArr = [
+    { name: '가루다', local: 'sin' },
+    { name: '타이탄', local: 'sin' },
+    { name: '이프리트', local: 'sin' },
+    { name: '모그', local: 'sin' },
+    { name: '리바', local: 'sin' },
+    { name: '라무', local: 'sin' },
+    { name: '시바', local: 'sin' },
+    { name: '오딘', local: 'sin' },
+
+    { name: '비스마르크', local: 'chang' },
+    { name: '라바나', local: 'chang' },
+    { name: '나오라', local: 'chang' },
+    { name: '세피', local: 'chang' },
+    { name: '니드호그', local: 'chang' },
+    { name: '소피아', local: 'chang' },
+    { name: '주르반', local: 'chang' },
+
+    { name: '스사노오', local: 'hong' },
+    { name: '락슈미', local: 'hong' },
+    { name: '신룡', local: 'hong' },
+    { name: '백호', local: 'hong' },
+    { name: '츠쿠', local: 'hong' },
+    { name: '리오레우스', local: 'hong' },
+    { name: '주작', local: 'hong' },
+    { name: '청룡', local: 'hong' },
+
+    { name: '티타니아', local: 'dark' },
+    { name: '이노센스', local: 'dark' },
+    { name: '하데스', local: 'dark' },
+    { name: '루비', local: 'dark' },
+    { name: '빛전', local: 'dark' },
+];
 
 // 프프로그 사용시 경고문
 const AlertText = new MessageEmbed()
@@ -215,16 +254,66 @@ client.on('message', async msg => {
     if (msg.content.startsWith(';;쿠로')) {
         const words = msg.content.replace(/^;;쿠로\s*/, '').split(' ');
 
-        for (let i = 0; i < words.length; i++) {
-            kuroArr.push(words[i]);
+        for(let i=0; i<=words.length; i++) {
+            const result = kuroListArr.filter(word => word.name.includes(words[i]));
+            kuroArr.push(...result);
         }
 
-        const setKuro = new Set(kuroArr);
-        const converterArray = JSON.stringify([...setKuro].join(", "));
+        const filterSin = kuroArr.filter(word => word.local === "sin");
+        const filterChang = kuroArr.filter(word => word.local === "chang");
+        const filterHong = kuroArr.filter(word => word.local === "hong");
+        const filterDark = kuroArr.filter(word => word.local === "dark");
 
-        converterArray.replace(/^"\s*/, '');
+        const Sin = Array.from(new Set(filterSin));
+        const Chang = Array.from(new Set(filterChang));
+        const Hong = Array.from(new Set(filterHong));
+        const Dark = Array.from(new Set(filterDark));
 
-        await msg.channel.send(`${converterArray} 쿠로 빙고 화이팅이야~ 쿠뽀!`);
+        Sin.map((data: any) => (
+            sinArr.push(data.name)
+        ));
+
+        Chang.map((data: any) => (
+            changArr.push(data.name)
+        ));
+
+        Hong.map((data: any) => (
+            hongArr.push(data.name)
+        ));
+
+        Dark.map((data: any) => {
+            darkArr.push(data.name)
+        });
+
+        if (sinArr.length != 0) {
+            let initSinArr = Array.from(new Set(sinArr));
+            msg.channel.send('신생: ' + initSinArr);
+            initSinArr = [];
+        }
+
+        if (changArr.length != 0) {
+            let initChangArr = Array.from(new Set(changArr))
+            msg.channel.send('창천: ' + initChangArr);
+            initChangArr = [];
+        }
+
+        if (hongArr.length != 0) {
+            let initHongArr = Array.from(new Set(hongArr))
+            msg.channel.send('홍련: ' + initHongArr);
+            initHongArr = [];
+        }
+
+        if (darkArr.length != 0) {
+            let initDarkArr = Array.from(new Set(darkArr));
+            msg.channel.send('칠흑: ' + initDarkArr);
+            initDarkArr = [];
+        }
+
+        kuroArr = [];
+        sinArr = [];
+        changArr = [];
+        hongArr = [];
+        darkArr = [];
     }
 
     // 지도 이미지를 바로 보여주는 기능
